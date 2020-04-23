96b8f1528ea34a9fed0b048dac47fa0499b7e601
yasser <yassermbb@gmail.com>
2020-04-23 08:46:49 -0300

    Start of project

---
+++ src/forLoops/Main.java
@@ -0,0 +1,92 @@
+package forLoops;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.infra.Blackhole;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@State(Scope.Benchmark)
+@Fork(value = 1, jvmArgs = { "-Xmx4G", "-XX:-UseBiasedLocking", "-XX:-AlwaysPreTouch" })
+public class Main {
+	private static List<Integer> a;
+
+	@Param({ "100000000" })
+	private static int N;
+
+	@Setup
+	public final void setup() {
+		a = createData();
+	}
+
+	public static void main(final String[] args) {
+		System.out.println(Main.class.getSimpleName());
+		final Options opt = new OptionsBuilder().include(Main.class.getSimpleName()).build();
+		try {
+			new Runner(opt).run();
+		} catch (final RunnerException e) {
+			System.out.println(e.getMessage());
+		}
+
+	}
+
+	@Benchmark
+	public final void foreachenchanced(final Blackhole bl) {
+		for (final var integer : a) {
+			bl.consume(integer);
+		}
+	}
+
+	@Benchmark
+	public final void forloop(final Blackhole bl) {
+		for (int i = 0; i < a.size(); i++) {
+			final var s = a.get(i);
+			bl.consume(s);
+		}
+	}
+
+	@Benchmark
+	public final void iterator(final Blackhole bl) {
+		for (final var iterator = a.iterator(); iterator.hasNext();) {
+			final var integer = iterator.next();
+			bl.consume(integer);
+		}
+	}
+
+	@Benchmark
+	public final void foreach(final Blackhole bl) {
+		a.forEach(e -> {
+			bl.consume(e);
+		});
+	}
+
+	private final List<Integer> createData() {
+		final var data = new ArrayList<Integer>(100_000_000);
+
+		for (int com = 0; com < N; com++) {
+			data.add(geraInt());
+		}
+
+		return data;
+	}
+
+	private final int geraInt() {
+		return ThreadLocalRandom.current().nextInt();
+	}
+}
---
+++ src/randomGenerators/Main.java
@@ -0,0 +1,116 @@
+package randomGenerators;
+
+import static java.util.stream.Collectors.toList;
+
+import java.util.ArrayList;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.infra.Blackhole;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@State(Scope.Benchmark)
+@Fork(value = 1, jvmArgs = { "-Xmx4G", "-XX:-UseBiasedLocking", "-XX:-AlwaysPreTouch" })
+public class Main {
+	
+	@Param({"10000000"})
+	private int ITERAIONS;
+	
+	public static void main(final String[] args) {
+		System.out.println(Main.class.getSimpleName());
+		final Options opt = new OptionsBuilder().include(Main.class.getSimpleName()).build();
+		try {
+			new Runner(opt).run();
+		} catch (final RunnerException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	
+	@Benchmark
+	public final void streamGenerator(Blackhole bl)
+	{
+		final var list = Stream.generate(this::randomNumber).limit(ITERAIONS).collect(toList());
+		bl.consume(list);
+	}
+	
+	@Benchmark
+	public final void intStreamGenerator(Blackhole bl)
+	{
+		//NOT SAFE FOR .parallel(), needs external sync of the ArrayList with Collections.synchronizedList()		
+		final var list = new ArrayList<Integer>();
+		IntStream.range(0, ITERAIONS).forEach(e -> {
+			list.add(randomNumber());
+		});;
+		bl.consume(list);
+	}
+	
+	@Benchmark
+	public final void forLoop(Blackhole bl)
+	{
+		final var list = new ArrayList<Integer>();
+		for (int i = 0; i < ITERAIONS; i++) {
+			list.add(randomNumber());
+		}
+		bl.consume(list);
+	}
+
+	@Benchmark
+	public final void intStreamIterate(Blackhole bl)
+	{
+		//NOT SAFE FOR .parallel(), needs external sync of the ArrayList with Collections.synchronizedList()	
+		final var list = new ArrayList<Integer>();
+		IntStream.iterate(0, i -> i + 1).limit(ITERAIONS).forEach(i ->	{
+			list.add(randomNumber());
+		});
+		bl.consume(list);
+	}
+	
+	@Benchmark
+	public final void streamIterate(Blackhole bl)
+	{
+		//NOT SAFE FOR .parallel(), needs external sync of the ArrayList with Collections.synchronizedList()	
+		final var list = new ArrayList<Integer>();
+		Stream.iterate(1, i -> i + 1).limit(ITERAIONS).forEach(i ->	{
+			list.add(randomNumber());
+		});
+		bl.consume(list);		
+	}
+	
+	@Benchmark
+	public final void intStreamGenerate(Blackhole bl)
+	{
+		final var list = IntStream.generate(this::randomNumber).limit(ITERAIONS).boxed().collect(toList());
+		bl.consume(list);
+	}
+	
+	@Benchmark
+	public final void threadRandomStream(Blackhole bl)
+	{
+		final var list = ThreadLocalRandom.current().ints(ITERAIONS).boxed().collect(toList());
+		bl.consume(list);
+	}
+	
+	
+	private final int randomNumber()
+	{
+		return ThreadLocalRandom.current().nextInt();
+	}
+	
+}
